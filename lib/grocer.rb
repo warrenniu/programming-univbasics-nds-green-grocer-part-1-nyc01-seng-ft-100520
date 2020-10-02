bdd1c14018f05d5f0b98c3d22b45e4687e812e3c
Warren Niu <warrenniu@gmail.com>
2020-10-02 14:35:50 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,6 +2,7 @@ def find_item_by_name_in_collection(name, collection)
   # Implement me first!
   #
   # Consult README for inputs and outputs
+  
 
 end
 
@@ -12,6 +13,3 @@ def consolidate_cart(cart)
   # change `cart` (i.e. mutate) it. It's easier to return a new thing.
 
 end
-
-
-  
